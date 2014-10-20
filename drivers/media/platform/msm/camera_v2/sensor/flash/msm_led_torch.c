e9dbbf6ffa08a4eb8f95be205fb3e39c517dd79f
Scott Mertz <inwotep@cyngn.com>
2014-10-19 19:44:24 -0700

    Merge yulong cp8675 camera driver to base camera driver
    Change-Id: I876a085d08db5e825393e46daf295a5480931836
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/sensor/Makefile
    	drivers/media/platform/msm/camera_v2/sensor/flash/Makefile
    	drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_torch.c
    	drivers/media/platform/msm/camera_v2/sensor/imx135.c
    	drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
    	drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
    	drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
    	drivers/media/platform/msm/camera_v2/sensor/ov5648.c

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_torch.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_torch.c
@@ -13,20 +13,63 @@
 
 #define pr_fmt(fmt) "%s:%d " fmt, __func__, __LINE__
 
+#ifdef CONFIG_MACH_YULONG
+#include <linux/gpio.h>
+#endif
 #include <linux/module.h>
 #include "msm_led_flash.h"
+#ifdef CONFIG_MACH_YULONG
+#include "msm_camera_io_util.h"
+#endif
 
+#ifndef CONFIG_MACH_YULONG
 static struct led_trigger *torch_trigger;
+#endif
+
+#ifdef CONFIG_MACH_YULONG
+static struct gpio torch_gpio[]={
+	{933,0,"FLASH"},
+	{934,0,"TORCH"},
+};
+#endif
 
 static void msm_led_torch_brightness_set(struct led_classdev *led_cdev,
 				enum led_brightness value)
 {
+#ifndef CONFIG_MACH_YULONG
 	if (!torch_trigger) {
 		pr_err("No torch trigger found, can't set brightness\n");
 		return;
 	}
 
 	led_trigger_event(torch_trigger, value);
+
+#else
+	int rc = 0;
+	rc = msm_camera_request_gpio_table(torch_gpio, 2, 1);
+	if (rc < 0) {
+		pr_err("%s: request gpio failed\n", __func__);
+		return;
+	}
+
+	if(value == 0) {
+		gpio_set_value_cansleep(torch_gpio[0].gpio, GPIO_OUT_LOW);
+		gpio_set_value_cansleep(torch_gpio[1].gpio, GPIO_OUT_LOW);
+	} else if (value > 0 && value <= 127) {
+		gpio_set_value_cansleep(torch_gpio[0].gpio, GPIO_OUT_LOW);
+		gpio_set_value_cansleep(torch_gpio[1].gpio, GPIO_OUT_HIGH);
+	} else if(value > 127 && value <= 255) {
+		gpio_set_value_cansleep(torch_gpio[0].gpio, GPIO_OUT_HIGH);
+		gpio_set_value_cansleep(torch_gpio[1].gpio, GPIO_OUT_LOW);
+	} else {
+		pr_err("%s: invalid value\n", __func__);
+	}
+
+	rc = msm_camera_request_gpio_table(torch_gpio, 2, 0);
+	if (rc < 0) {
+		pr_err("%s: free gpio failed\n", __func__);
+	}
+#endif
 };
 
 static struct led_classdev msm_torch_led[MAX_LED_TRIGGERS] = {
@@ -51,19 +94,18 @@ int32_t msm_led_torch_create_classdev(struct platform_device *pdev,
 				void *data)
 {
 	int32_t i, rc = 0;
+#ifndef CONFIG_MACH_YULONG
 	struct msm_led_flash_ctrl_t *fctrl =
 		(struct msm_led_flash_ctrl_t *)data;
 
-	if (!fctrl) {
-		pr_err("Invalid fctrl\n");
+	if (!fctrl || !fctrl->torch_trigger) {
+		pr_err("Invalid fctrl or torch trigger\n");
 		return -EINVAL;
 	}
 
-	for (i = 0; i < fctrl->torch_num_sources; i++) {
-		if (fctrl->torch_trigger[i]) {
-			torch_trigger = fctrl->torch_trigger[i];
-			msm_led_torch_brightness_set(&msm_torch_led[i],
-				LED_OFF);
+	torch_trigger = fctrl->torch_trigger;
+#endif
+	msm_led_torch_brightness_set(&msm_torch_led, LED_OFF);
 
 			rc = led_classdev_register(&pdev->dev,
 				&msm_torch_led[i]);
